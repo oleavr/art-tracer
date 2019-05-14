044858b4dffb4a71ba4c8d9c1192cb404417cf44
lavoisier wapet <lavoisierwapet@gmail.com>
2019-05-14 11:20:46 +0200

    fixing compilation errors

--- agent/shims-frida-java.d.ts
+++ agent/shims-frida-java.d.ts
@@ -1,7 +1,43 @@
-interface JavaApi {
-    artRuntime: NativePointer;
+
+interface VM {
+    perform(function_: any): any;
+    getEnv(): NativePointer;
 }
 
-declare module "frida-java/lib/android" {
-    function getApi(): JavaApi;
+declare module "frida-java/lib/vm" {
+    interface VMConstructor {
+        new(api: JavaApi): VM;
+     }
+    const VM: VMConstructor;
+    export = VM;
 }
+
+
+
+
+interface JavaApi {
+    artRuntime: NativePointer;  
+    vm: NativePointer;  
+}
+
+declare module "frida-java" {
+    function perform(f: () => void): void;
+}
+
+
+declare module "frida-java/lib/android" {
+    function getApi(): JavaApi;   
+    function getArtThreadFromEnv(env: NativePointer): NativePointer; 
+}
+
+
+/*declare class VM{
+    constructor(api: JavaApi);
+    perform(function_: any): any;
+}
+
+declare module "frida-java/lib/vm" {
+     VM;
+}*/
+
+declare const X86Writer: any;
